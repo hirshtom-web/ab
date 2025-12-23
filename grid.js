20cadf0e183a152f06be1796bcb3f1e83a21e126
hirshtom-web <hirshtom@gmail.com>
2025-12-23 11:42:17 +0200

    Update grid.js

--- grid.js
+++ grid.js
@@ -53,11 +53,9 @@ function initProductsPage() {
 
       card.innerHTML = `
         <div class="mockup-stage">
-          <div class="poster-frame">
               <div class="artwork">
                 <img src="${firstImageUrl}" alt="${p.name}">
               </div>
-            </div>
         </div>
         <div class="product-info">
           <h3>${p.name}</h3>
