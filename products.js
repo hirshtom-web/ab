f90da995fb3dd939f83359ddf4ee3c7c3d1a0d94
hirshtom-web <hirshtom@gmail.com>
2025-12-28 00:29:11 +0200

    Update products.js

--- products.js
+++ products.js
@@ -6,7 +6,7 @@ function initProductsPage() {
   let allProducts = [];
   let currentIndex = 0;                  
   const initialLoad = 40;
-  const loadMoreCount = 18; 
+  const loadMoreCount = 18;
   let currentImageIndex = parseInt(localStorage.getItem("gridImageIndex")) || 0; ?
 
   const banners = [
