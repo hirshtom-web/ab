14104e4842d20ba46647a903dddb695fb345c63a
hirshtom-web <hirshtom@gmail.com>
2025-12-29 00:55:24 +0200

    Update related-slider.js

--- related-slider.js
+++ related-slider.js
@@ -1,53 +1,53 @@
-// Example: IDs of related products (or dynamically from category)
-const relatedProductIds = [
-  "9182888",
-  "9182889",
-  "9182890",
-  "9182891",
-  "9182892",
-];
+function initRelatedSlider() {
+  const relatedProductIds = [
+    "9182888",
+    "9182889",
+    "9182890",
+    "9182891",
+    "9182892",
+  ];
 
-const sliderContainer = document.getElementById("relatedProductsSlider");
+  const sliderContainer = document.getElementById("relatedProductsSlider");
+  if (!sliderContainer) return; // Exit if container doesn't exist
 
-fetch("https://hirshtom-web.github.io/ab/product-catalog.csv")
-  .then(res => res.text())
-  .then(csvText => {
-    const data = Papa.parse(csvText, { header: true, skipEmptyLines: true }).data;
+  fetch("https://hirshtom-web.github.io/ab/product-catalog.csv")
+    .then(res => res.text())
+    .then(csvText => {
+      const data = Papa.parse(csvText, { header: true, skipEmptyLines: true }).data;
+      const products = data.filter(p => relatedProductIds.includes(p.productId));
 
-    // Filter related products
-    const products = data.filter(p => relatedProductIds.includes(p.productId));
+      products.forEach(p => {
+        const mainImages = (p.mainImageUrl || "")
+          .split(";")
+          .map(i => i.trim())
+          .filter(Boolean);
 
-    products.forEach(p => {
-      const mainImages = (p.mainImageUrl || "")
-        .split(";")
-        .map(i => i.trim())
-        .filter(Boolean);
+        const imgSrc = mainImages[0]
+          ? (mainImages[0].includes("http")
+              ? mainImages[0]
+              : "https://static.wixstatic.com/media/" + mainImages[0])
+          : "";
 
-      const imgSrc = mainImages[0]
-        ? (mainImages[0].includes("http")
-            ? mainImages[0]
-            : "https://static.wixstatic.com/media/" + mainImages[0])
-        : "";
+        const item = document.createElement("div");
+        item.className = "related-slider-item";
 
-      const item = document.createElement("div");
-      item.className = "related-slider-item";
+        item.innerHTML = `
+          <div class="img-wrapper">
+            <img src="${imgSrc}" alt="${p.name}">
+          </div>
+          <div class="related-slider-info">
+            <h4>${p.name}</h4>
+            <span class="price">$${parseFloat(p.newPrice || 0).toFixed(2)}</span>
+          </div>
+        `;
 
-      item.innerHTML = `
-        <div class="img-wrapper">
-          <img src="${imgSrc}" alt="${p.name}">
-        </div>
-        <div class="related-slider-info">
-          <h4>${p.name}</h4>
-          <span class="price">$${parseFloat(p.newPrice || 0).toFixed(2)}</span>
-        </div>
-      `;
+        item.addEventListener("click", () => {
+          window.location.href =
+            `https://hirshtom-web.github.io/ab/product-page.html?id=${p.productId}`;
+        });
 
-      // Link to product page
-      item.addEventListener("click", () => {
-        window.location.href =
-          `https://hirshtom-web.github.io/ab/product-page.html?id=${p.productId}`;
+        sliderContainer.appendChild(item);
       });
-
-      sliderContainer.appendChild(item);
-    });
-  });
+    })
+    .catch(err => console.error("Related slider failed:", err));
+}
