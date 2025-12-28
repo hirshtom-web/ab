d3c07418c4106b3e0fe46c451fd78cb5d18a07f7
hirshtom-web <hirshtom@gmail.com>
2025-12-28 16:37:15 +0200

    Refactor HTML structure in product display

--- products.js
+++ products.js
@@ -218,11 +218,7 @@ loadMoreProducts();
       const priceHTML = p.oldPrice && p.oldPrice > p.price
         ? `<span class="price-from">$${p.price.toFixed(2)}</span><span class="price-old">$${p.oldPrice.toFixed(2)}</span>`
         : `<span class="price-new">$${p.price.toFixed(2)}</span>`;
-
-     <div class="img-wrapper">
-  ${discountBubble}
-  <img src="${imgSrc}" alt="${p.name}">
-</div>
+        <div class="img-wrapper">${discountBubble}<img src="${imgSrc}" alt="${p.name}"></div>
         <div class="product-info">
           <h3>${p.name}</h3>
           <div class="price-wrapper">${priceHTML}</div>
